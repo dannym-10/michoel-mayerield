2d125dbadea894f8a5724925b285c3b14e4a54b5
Danny Moss <dannymoss@Dannys-MacBook-Pro-2.local>
2023-10-21 01:09:28 +0100

    updates folder structure

--- micky/src/components/Header/index.tsx
+++ micky/src/components/Header/index.tsx
@@ -4,6 +4,7 @@ import { Squash as Hamburger } from "hamburger-react";
 import "./Header.scss";
 import { useLocation } from "react-router-dom";
 import { Spring } from "../Spring";
+import { Day } from "date-fns";
 
 interface HeaderProps {}
 
@@ -25,6 +26,7 @@ export const Header: React.FC<HeaderProps> = () => {
             setShowNavigation((prevShowNavigation) => !prevShowNavigation)
           }
         >
+          {}
           <Hamburger />
         </div>
         <div className="header-wrapper__logo">
